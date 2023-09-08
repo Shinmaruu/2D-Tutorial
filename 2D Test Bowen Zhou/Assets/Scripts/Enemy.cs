2f4140214d2551a71b6bc1e5df9af04febdb8f40
Shinmaruu <bdzhou@iu.edu>
2023-09-07 22:12:56 -0400

    The entire 2D Tutorial Project

---
+++ 2D Test Bowen Zhou/Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float speed = 15f;
+    [SerializeField] GameManager manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0, speed, 0) * Time.deltaTime;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //        Debug.Log("Collision");
+        if(collision.gameObject.tag == "Player")
+        {
+            GameManager.instance.InitiateGameOver();
+        }
+        else
+        {
+        GameManager.instance.IncreaseScore(10);
+        }
+        Destroy(gameObject);
+        Destroy(collision.gameObject);
+    }
+}
